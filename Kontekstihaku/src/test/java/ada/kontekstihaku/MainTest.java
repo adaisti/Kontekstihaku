06a1b268f3717f249500a02b15c0994676b8c338
adaisti <Ada@AdaMaarnMacBook.home>
2015-03-12 22:45:28 +0200

    projekti luotu

---
+++ Kontekstihaku/src/test/java/ada/kontekstihaku/MainTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ada.kontekstihaku;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ada
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+     @Test
+    public void testMainPalikka() {
+        assertTrue(true);
+    }
+    
+    
+}
